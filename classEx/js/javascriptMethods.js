30ddc983ba5d71ed8cc4921adbec6fca189a39e4
BREANN NIELSEN <breannielsen@gmail.com>
2020-05-11 13:02:57 -0700

    strings

--- classEx/js/javascriptMethods.js
+++ classEx/js/javascriptMethods.js
@@ -3,6 +3,21 @@ var str = 'this is a string.';
 // length method - returns size of the string
 var strLen = str.length;
 
+var strIdx = str[8];
+
+// indexOf method - get position of character in string
+var strIdxOf = str.indexOf(5);
+
+// slice method - get a substring or specified portion of a string.center
+var strSlice = str.slice(5,7);
+
+// toUpperCase() and toLowerCase() - change the case of a string character
+var strUpper = str.toUpperCase();
+var strLower = str.toLowerCase();
+
+// replace() method = replace character of a string 
+var strRpl = str.replace();
+
 
 
 

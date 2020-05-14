17d06a336e3b144bbf4a2f39a8b9fdc218ef908a
Breann <breannielsen@gmail.com>
2020-05-14 12:51:54 -0700

    added var and switch

---
+++ classEx/Sandbox/switchStatementExample.js
@@ -0,0 +1,8 @@
+// Switch Statement - gives multiple outcomes based on a value of condition 
+
+var seasons = 'Spring';
+
+switch(seasons){
+    case 'Summer':
+        console.log('It\'s Summer!')
+}

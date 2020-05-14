bde58835544dc861c13ce3777898a37f4fccdf93
Breann <breannielsen@gmail.com>
2020-05-14 12:55:59 -0700

    added case 'Summer'

--- classEx/Sandbox/switchStatementExample.js
+++ classEx/Sandbox/switchStatementExample.js
@@ -5,4 +5,17 @@ var seasons = 'Spring';
 switch(seasons){
     case 'Summer':
         console.log('It\'s Summer!')
+        break;
+    case 'Summer':
+        console.log('It\'s Summer!')
+        break;
+    case 'Summer':
+            console.log('It\'s Summer!')
+            break;
+    case 'Summer':
+                console.log('It\'s Summer!')
+                break;
+            default:
+                console.log('There are only 4 seasons!');
+                break;
 }

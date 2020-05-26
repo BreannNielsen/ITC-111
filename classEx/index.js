f5ffbae95c9093d79a2afc4fe76bcc0b44f070d6
Breann <breannielsen@gmail.com>
2020-05-26 13:39:46 -0700

    lots of edits

--- classEx/index.js
+++ classEx/index.js
@@ -10,21 +10,23 @@
         document.getElementById('divLeft_Default').style.display = "block";
         document.getElementById('divLeft_CELinks').style.display = "none";
         document.getElementById('divLeft_ALinks').style.display = "none";
-        document.getElementById('divRight').style.display = "none";
         document.getElementById('ls_Heading').innerHTML = "Home Link clicked. Default text displayed.";
+        document.getElementById('ls_Heading').innerHTML = "ITC-111";
+        document.getElementById('rs_Heading').innerHTML = "Intro to Programming <br>w/HTML, CSS & JavaScript</a>";
+
     }else if (lnk == 'classEx'){
         document.getElementById('divLeft_Default').style.display = "none";
         document.getElementById('divLeft_CELinks').style.display = "block";
         document.getElementById('divLeft_ALinks').style.display = "none";
-        document.getElementById('divRight').style.display = "none";
-        document.getElementById('ls_Heading').innerHTML = "Class Exercises";
+        document.getElementById('ls_Heading').innerHTML = "Default content for Class Exercise Link.";
+        document.getElementById('ls_Heading').innerHTML = "Class Exercise Links";
         document.getElementById('rs_Heading').innerHTML = "Exercise Description";
 
     }else if (lnk == 'assign'){
         document.getElementById('divLeft_Default').style.display = "none";
         document.getElementById('divLeft_CELinks').style.display = "none";
         document.getElementById('divLeft_ALinks').style.display = "block";
-        document.getElementById('divRight').style.display = "none";
+        document.getElementById('ls_Heading').innerHTML = "Default description for Assignments Link.";
         document.getElementById('ls_Heading').innerHTML = "Assignments";
         document.getElementById('rs_Heading').innerHTML = "Assignment Description";
 

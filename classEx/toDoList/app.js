77146eea10cab90c03c8cbe94fd530bed184474a
Breann <breannielsen@gmail.com>
2020-06-09 13:48:47 -0700

    updates

--- classEx/toDoList/app.js
+++ classEx/toDoList/app.js
@@ -9,6 +9,10 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+// Variables 
+let LIST = []
+    , id = 0;
+
 // Show today's date
 const options = {weekday : "long", month:"short", day:"numeric"};
 const today = new Date();
@@ -24,9 +28,9 @@ function addToDo(toDo, id, done, trash){
     const DONE = done ? CHECK : UNCHECK;
     const LINE = done ? LINE_THROUGH : "";
     const item = `<li class="item">
-                    <i class="fa fa-circle-thin co" job="complete" id="0"></i>
-                    <p class="text">Drink Coffee</p>
-                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text" ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
                     </li>
                 `;
 
@@ -42,9 +46,34 @@ document.addEventListener("keyup",function(even){
 
         // if the input isn't empty
         if(toDo){
-            addToDo(toDo);
+            addToDo(toDo, id, false, false);
+            
+            LIST.push){
+                name : toDo?
+                id : id,
+                done : false,
+                trash : false
+            });
+
+            id++;
         }
         input.value = "";
     }
 
 });
+
+// Complete to-do
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// Remove to-do
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
